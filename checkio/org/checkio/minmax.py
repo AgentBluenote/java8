fe48765494508fbc4b8a5f00e7e986d779976349
Michael <monolisa56@gmail.com>
2015-11-14 13:41:52 -0500

    working copy with print debug statements

--- checkio/org/checkio/minmax.py
+++ checkio/org/checkio/minmax.py
@@ -5,88 +5,127 @@ Created on Nov 4, 2015
 '''
 
 def my_min(*args, **kwargs):
-#   key = kwargs.get("key", None)
-    
-    print(args)
-    rv = list(args) 
-    print(rv)
-    rv.sort(key=None, reverse=False)
+    """Docstring."""
 
-#   rv.sort(key=key, reverse=False)
-#   rv.sort(key=key, reverse=False)
-#   Any formal parameters which occur after the args parameter are keyword-only arguments.
-    print(rv)
+    print("Inside my_min()")    
+
+    key = kwargs.get("key", None)
+    #   D.get(k[,d]) -> D[k] if k in D, else d.  d defaults to None.
+    
+    if key: # has "key" keyword argument 
+        print("keyword arg key =", key )
+    else: # key is None
+        # lets make keyword argument so we can treat logic to find min 
+        # in a generic fasion 
+        key = lambda x: x 
+        print("NO keyword arg key = ", key)
+
+    print("args = ",  len(args))
+
+    if len(args) > 1:   # positional arguments packed in tuple 
+        arglist = list(args)
+    else:  
+        arglist = args[0]  # pull sequence out of tuple
+
+    print("arglist = " , arglist)
+
+    minval = key(arglist[0])  # initial value
+
+    print("minval init = ", minval)
+
+    index = 0
+    rv = arglist[0]
+    for i in arglist:   # goes through sequence: could be 1,2,3 or [1,2],[4,5]
+        mapvalue = key(i)
+        print("mapvalue - ", mapvalue)
+
+        if key(i) < minval:
+            minval = key(i)
+            rv = arglist[index]
+            print("rv = 1", rv)
+
+        print("index: ",index)
+        print("i:",i)
+        print("minval = ", minval)
+
+        index = index + 1 
 
     return rv
-    
-     
 
+    
 def my_max(*args, **kwargs):
     """Docstring."""
 
     print("Inside my_max()")    
 
-    keyval = kwargs.get("key", None)
-
-    if keyval: # key is None 
-        print("keyword arg key =", keyval )
-    else:
-        print("NO keyword arg key = ", keyval)
-
+    key = kwargs.get("key", None)
+    #   D.get(k[,d]) -> D[k] if k in D, else d.  d defaults to None.
     
-#   key=str.lower
-#   print(key)
+    if key: # has "key" keyword argument 
+        print("keyword arg key =", key )
+    else: # key is None
+        # lets make keyword argument so we can treat logic to find max 
+        # in a generic fasion 
+        key = lambda x: x 
+        print("NO keyword arg key = ", key)
 
-    try:
-        iterator = iter(args)
-        print("Iterator type")
-#       print(iterator)
+    print("args = ",  len(args) ) 
 
-#       arglist = list(args)
-#       arglist.sort(key=keyval)
+    if len(args) > 1:   # positional arguments packed in tuple 
+        arglist = list(args)
+    else:  
+        arglist = args[0]  # pull sequence out of tuple
 
-        test = tuple()
-        le= test.__len__()
+    print("arglist = " , arglist)
 
+    maxval = key(arglist[0])  # initial value
 
-        arglist = args  # unpack
-        print("arglist len =",arglist.__len__())
-        print(arglist)
-#       arglist.sort(key=keyval)
+    print("maxval init = ", maxval)
 
-#       retval = arglist.pop()
-        
-    except TypeError:
-        # not iterable
-        iterator = False
+    index = 0
+    rv = arglist[0]
+    for i in arglist:   # goes through sequence: could be 1,2,3 or [1,2],[4,5]
+        mapvalue = key(i)
+        print("mapvalue - ", mapvalue)
 
-        print("print arglist ", arglist)
-        print(arglist) 
-    else:
-        # iterable
-        for obj in iterator:
-            pass
+        if key(i) > maxval:
+            maxval = key(i)
+            rv = arglist[index]
+            print("rv = 1", rv)
 
-    return 2 
+        print("index: ",index)
+        print("i:",i)
+        print("maxval = ", maxval)
 
+        index = index + 1 
 
+    return rv
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
     
     val = my_max(3,2,5,8,key=int)
-    print(val)
+    print("*****************************")
+    print("*****************************")
+    print("*****************************")
 
     my_max( [1, 2, 0, 3, 4] )
+    print("*****************************")
+    print("*****************************")
+    print("*****************************")
+    my_max("hello")
+
+    print("*****************************")
+    print("*****************************")
+    print("*****************************")
+    my_max( [[1, 2], [3, 4], [9, 0]] )
 
 
-#   val2 = my_max(3,key=2)
-#   print(val2)
-
-#   assert my_max(3, 2) == 3, "Simple case max"
-#   assert my_min(3, 2) == 2, "Simple case min"
-#   assert my_max([1, 2, 0, 3, 4]) == 4, "From a list"
-#   assert my_min("hello") == "e", "From string"
-#   assert my_max(2.2, 5.6, 5.9, key=int) == 5.6, "Two maximal items"
-#   assert my_min([[1, 2], [3, 4], [9, 0]], key=lambda x: x[1]) == [9, 0], "lambda key"
+    assert my_max(3, 2) == 3, "Simple case max"
+    assert my_min(3, 2) == 2, "Simple case min"
+    assert my_max([1, 2, 0, 3, 4]) == 4, "From a list"
+    assert my_min("hello") == "e", "From string"
+    assert my_max(2.2, 5.6, 5.9, key=int) == 5.6, "Two maximal items"
+    assert my_min([[1, 2], [3, 4], [9, 0]], key=lambda x: x[1]) == [9, 0], "lambda key"
+    assert my_max([[1, 2], [3, 4], [9, 0]], key=lambda x: x[1]) == [3, 4], "lambda key"
 
