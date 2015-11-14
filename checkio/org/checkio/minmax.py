e991a042f432e9120c2175d86b06cde37f968801
Michael <monolisa56@gmail.com>
2015-11-14 14:59:28 -0500

    change function signiture from my_min() my_max() to min() and max()

--- checkio/org/checkio/minmax.py
+++ checkio/org/checkio/minmax.py
@@ -4,10 +4,10 @@ Created on Nov 4, 2015
 @author: Michael
 '''
 
-def my_min(*args, **kwargs):
+def min(*args, **kwargs):
     """Docstring."""
 
-    print("Inside my_min()")    
+    print("Inside min()")    
 
     key = kwargs.get("key", None)
     #   D.get(k[,d]) -> D[k] if k in D, else d.  d defaults to None.
@@ -53,10 +53,10 @@ def my_min(*args, **kwargs):
     return rv
 
     
-def my_max(*args, **kwargs):
+def max(*args, **kwargs):
     """Docstring."""
 
-    print("Inside my_max()")    
+    print("Inside max()")    
 
     key = kwargs.get("key", None)
     #   D.get(k[,d]) -> D[k] if k in D, else d.  d defaults to None.
@@ -104,28 +104,29 @@ def my_max(*args, **kwargs):
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
     
-    val = my_max(3,2,5,8,key=int)
+    val = max(3,2,5,8,key=int)
     print("*****************************")
     print("*****************************")
     print("*****************************")
 
-    my_max( [1, 2, 0, 3, 4] )
+    max( [1, 2, 0, 3, 4] )
     print("*****************************")
     print("*****************************")
     print("*****************************")
-    my_max("hello")
+    max("hello")
 
     print("*****************************")
     print("*****************************")
     print("*****************************")
-    my_max( [[1, 2], [3, 4], [9, 0]] )
+    max( [[1, 2], [3, 4], [9, 0]] )
 
 
-    assert my_max(3, 2) == 3, "Simple case max"
-    assert my_min(3, 2) == 2, "Simple case min"
-    assert my_max([1, 2, 0, 3, 4]) == 4, "From a list"
-    assert my_min("hello") == "e", "From string"
-    assert my_max(2.2, 5.6, 5.9, key=int) == 5.6, "Two maximal items"
-    assert my_min([[1, 2], [3, 4], [9, 0]], key=lambda x: x[1]) == [9, 0], "lambda key"
-    assert my_max([[1, 2], [3, 4], [9, 0]], key=lambda x: x[1]) == [3, 4], "lambda key"
-
+    assert max(3, 2) == 3, "Simple case max"
+    assert min(3, 2) == 2, "Simple case min"
+    assert max([1, 2, 0, 3, 4]) == 4, "From a list"
+    assert min("hello") == "e", "From string"
+    assert max(2.2, 5.6, 5.9, key=int) == 5.6, "Two maximal items"
+    assert min([[1, 2], [3, 4], [9, 0]], key=lambda x: x[1]) == [9, 0], "lambda key"
+    assert max([[1, 2], [3, 4], [9, 0]], key=lambda x: x[1]) == [3, 4], "lambda key"
+#   assert min(abs(i) for i in range(-10, 10)) == 0, "Generator"
+#   assert max(abs(i) for i in range(-10, 10)) == 10, "Generator"
