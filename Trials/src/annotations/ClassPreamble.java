e05644bbeeb56ae63f1aba4e222f725190ac1bde
Michael <Michael@Michael-PC>
2016-10-15 12:55:32 -0400

    put ClassPreamble.java into annotations package

---
+++ Trials/src/annotations/ClassPreamble.java
@@ -0,0 +1,22 @@
+package annotations;
+import java.lang.annotation.Documented;
+
+
+/**
+*  Mike: This is how you declare annotations
+* 
+* @author      Michel AA 
+* @version     %I%, %G%
+* @since       1.0
+*
+*/
+@Documented
+public @interface ClassPreamble {  
+    String author();        
+	String date();
+//	int currentRevision() default 1;       
+	String lastModified() default "N/A";    // MIKE: "annotation type elements" can have a default.   
+	String lastModifiedBy() default "N/A";
+	// Note use of array
+	String[] reviewers();
+}
