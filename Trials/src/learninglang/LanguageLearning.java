b496a39dc2c0883d787dc506647e634f9264b2d4
Michael <Michael@Michael-PC>
2017-01-07 19:28:35 -0500

    messing around with Class.cast() method camparing to instanceof operator

--- Trials/src/learninglang/LanguageLearning.java
+++ Trials/src/learninglang/LanguageLearning.java
@@ -7,13 +7,22 @@ public class LanguageLearning {
 		// defualt constructor
 	}
 	
-	public static void test1(){
+	public static void testMaxValueOfInteger(){
+		@SuppressWarnings("unused")
 	    int maxInt = 2147483647;	
+
+		System.out.println("\n** Inside testMaxValueOfInteger\n");
 		
 		System.out.println("Integer max = " + Integer.MAX_VALUE);
 		System.out.println("Integer max = " + Integer.MIN_VALUE);
-
-	
+		
+		/*
+		 *  MIKE TEST RESULTS:  
+		 *
+		 *  maximun value an int can hold is 2**31  
+		 *  java uses one of the 32 bits as a sign bit
+	 	 */
+		
 	}
 
 	public static void labeledBreaktest(){
@@ -22,13 +31,15 @@ public class LanguageLearning {
 	            { 32, 87, 3, 589 },
 	            { 12, 1076, 2000, 8 },
 	            { 622, 127, 77, 955 }
-	        };
-	        int searchfor = 12;
+        };
+        int searchfor = 12;
 
-	        int i;
-	        int j = 0;
-	        boolean foundIt = false;
+        int i;
+        int j = 0;
+        boolean foundIt = false;
 	        
+		System.out.println("\n** Inside labeledBreaktest\n");
+
 	    //  MIKE:  labels allow you to exit more than one logical block    
 	    search:
 	        for (i = 0; i < arrayOfInts.length; i++) {
@@ -48,14 +59,59 @@ public class LanguageLearning {
 	        }
 	}
 
+	public static void testClassisInstanceMethod(){
+		Animal       catModel =  new Animal();
+		Animal       cat =  new Animal();
+		ClonedAnimal clonedCat = new ClonedAnimal();
+
+		System.out.println("\n** Inside testClassisInstanceMethod\n");
+		
+
+
+		if( clonedCat instanceof Animal )
+			 cat.printAnimalType();
+		if( clonedCat instanceof ClonedAnimal)
+			clonedCat.printAnimalType();
+
+
+
+		/*
+		 *  MIKE:  this is just pulling "Class Blue-Print"
+		 */
+		Class<?> model = catModel.getClass();
+
+		/*
+		 *  MIKE TEST RESULTS:  
+		 *
+		 *  isInstance is the "dynamic equivelant" of the instanceof operator.
+		 *  Look into this to eliminate "switch logic" 
+		 *  Research this a little more...
+	 	 */
+		// parent class
+		if( model.isInstance(cat) )
+			cat.printAnimalType();
+
+		// child class
+		if( model.isInstance(clonedCat) )
+			clonedCat.printAnimalType();
+	}
+
 	public static void main(String[] args) {
 
-		LanguageLearning.test1();
+		LanguageLearning.testMaxValueOfInteger();
 		
 		// illistates labeled break
 		// continue works with labels as well 
 		LanguageLearning.labeledBreaktest();
 		
+		/*
+		 *  Test Class.isInstance() method
+		 */
+		LanguageLearning.testClassisInstanceMethod();
+		
+		
+		
 	}
-
+	
+	
 }
