019ff8f08bf04e6121bba62d300bfd6633339a46
Michael <Michael@Michael-EEE.home>
2018-09-06 22:15:04 -0700

    added some stuff

--- Trials/src/drivers/Pharmacy_ABC.java
+++ Trials/src/drivers/Pharmacy_ABC.java
@@ -1,50 +1,66 @@
 package drivers;
 
-import java.lang.*;  
+import java.lang.*;
+import java.net.HttpURLConnection;
 import java.io.*;
+import java.util.Calendar;
+import java.util.Locale;
 
 
-/*
- *  Client/Server program for the 
- *  to connect you need to:
- *    know which port.
- *    
- *   thing about the time server. 
- */
+//
+//  Client/Server program for the 
+//
+//
 public class Pharmacy_ABC {
 
-
+    // 
+    // default Constructor
+    // 
 	public Pharmacy_ABC(){
 
 		// 
 		// we will abstract out foreign strains here.
 		// 
 	    System.out.println("Pharmacy_ABC");
-	    System.out.println("Pharmacy_ABC");
-	    System.out.println("Pharmacy_ABC");
-	    System.out.println("Pharmacy_ABC");
-	    System.out.println("Pharmacy_ABC");
 	}
 
+    //
+    //
+    //
 	public void populate_DB(){ 
-		// 
-		// we will abstract out foreign strains here.
-		// 
-	    System.out.println("Pharmacy_ABC");
+
+	    System.out.println("populate_DB");
+	    System.out.println("We like uppers, downers and women!! ");
 	}
 
+    //
+    //
+    //
+	public void timing(){ 
+    	Locale url = Locale.US; 
+    	Calendar timeStamp = Calendar.getInstance();
+        
+	}
+
+    //
+    // main()
+    //
 	public static void main( String[] args ) {
 
 	    Pharmacy_ABC bigPharma = new Pharmacy_ABC();  
 
 	    bigPharma.populate_DB();
+	    bigPharma.serverSide();
+	    bigPharma.timing();
 	}
 	
+	//
     public void serverSide(){
-    	
-    //	String = url 
-    //	HttpURLConnection connect = new HttpURLConnection(url);
-    	
+
+
+//		HttpURLConnection connect = new HttpURLConnection( url );
+
+	    System.out.println("serverSide()");
     }	
 }
 
